ba9f8c88c4e49c71c41107382244080906a65b1f
sainikunal5950-sudo <sainikunal5950@gmail.com>
2026-08-18 19:33:32 +0530

    start

--- 01-what-is-nodejs/basics.js
+++ 01-what-is-nodejs/basics.js
@@ -1,1 +1,15 @@
 console.log("CHUTIYA HAI TU BHAI ");
+
+
+// node js hme kuch specific eneviornment deta hai  jais eki  current folder ka path 
+
+console.log("Current directory:", __dirname);
+
+console.log("Current file:", __filename);
+
+
+const os = require("os");
+
+console.log("operating system ", os.platform());
+console.log("CPU architecture ", os.arch());
+console.log("Number of cpus", os.cpus().length);
